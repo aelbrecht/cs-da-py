9a97d031c95af6d57492c93e0787c63c76315558
Rudolf Aelbrecht <aelbrechtrudolf@gmail.com>
2022-02-27 15:04:35 +0100

    add ostergard

--- clique/ostergard.py
+++ clique/ostergard.py
@@ -3,7 +3,7 @@ import time
 
 def load():
     vertices = {}
-    with open("./input/p_hat300-1.clq") as f:
+    with open("./input/c-fat200-2.clq") as f:
         for line in f:
             if line[0] != "e":
                 continue
@@ -22,26 +22,43 @@ def intersection(l1, l2):
 
 
 def clique(us: list, size):
-    global maximum
+    global maximum, found, c
     if len(us) == 0:
         if size > maximum:
             maximum = size
             # new record; save it.
+            found = True
         return
     while len(us) != 0:
+
         if size + len(us) <= maximum:
             return
+
         i = us[0]
+
+        if size + c[i] <= maximum:
+            return
+
         us = us[1:]
         clique(intersection(us, edges[i]), size + 1)
+        if found is True:
+            return
 
 
 if __name__ == '__main__':
     edges = load()
     maximum = 0
     vxs = sorted(list(edges.keys()))
+    n = len(vxs)
     start_time = time.time()
-    clique(vxs, 0)
+    c = {}
+    for k in vxs:
+        c[k] = 0
+    for j in range(n - 1, -1, -1):
+        found = False
+        k = vxs[j]
+        clique(intersection(vxs[j:], edges[k]), 1)
+        c[k] = maximum
+    print(maximum)
     elapsed = time.time() - start_time
     print(f"took {elapsed}s")
-    print(maximum)
