00a1061ae63e83ae93b222413a546fece190c750
Rudolf Aelbrecht <aelbrechtrudolf@gmail.com>
2022-03-21 17:43:05 +0100

    add partial algorithm for fast_wvc

--- wvcp/fwvc.py
+++ wvcp/fwvc.py
@@ -1,4 +1,5 @@
 import random
+import time
 from typing import Type
 
 from loader import generate_graph, Graph
@@ -17,6 +18,16 @@ def largest_degree(x1: int, x2: int, g: Type[Graph]):
         return x1
     else:
         return x2
+
+
+def d_score(c: set[int], v: int, g: Type[Graph]):
+    c_bis = c.copy()
+    c_bis.remove(v)
+    return cost(c, g) - cost(c_bis, g)
+
+
+def loss(c: set[int], v: int, g: Type[Graph]):
+    return abs(d_score(c, v, g)) / g.weights[v]
 
 
 def construct_wvc(g: Type[Graph]) -> set[int]:
@@ -63,14 +74,72 @@ def construct_wvc(g: Type[Graph]) -> set[int]:
     return c
 
 
+def is_edge_uncovered(c: set[int], g: Type[Graph]) -> bool:
+    # TODO
+    return True
+
+
 def fast_wvc(g: Type[Graph]):
     c = construct_wvc(g)
     c_star = c.copy()
 
-    # TODO
+    edge_w = [1] * len(g.edges)
+    conf_change = [1] * len(g.vertices)
+
+    # calculate gain and loss of vertices?
+    tabulist = set()
+    start_time = time.time()
+    elapsed_time = 0
+    while elapsed_time < cutoff:
+        elapsed_time = time.time() - start_time
+
+        # choose a vertex w with minimum loss from C, breaking ties in favor of the oldest one;
+        w = -1
+        minimum = 1e10
+        for v in c:
+            y = loss(c, v, g)
+            if y < minimum:
+                w = v
+                minimum = y
+        c.remove(w)
+        conf_change[w] = 0
+        for z in g.neighbors[w]:
+            conf_change[z] = 1
+
+        # choose a vertex u with tabu[u] = 0 from C according to BMS strategy, breaking ties in favor of the oldest one;
+        # TODO
+        # C ← C \ {u};
+        # con fChange(u ) ← 0, con fChange(z ) ← 1 for each vertex z ∈ N (u );
+        # tabulist ← ∅;
+
+        # some edge is uncovered by C
+        while is_edge_uncovered(c, g):
+            # choose a vertex v, whose con fChange(v ) = 1, with maximum gain from V \ C, breaking ties in favor of the
+            # oldest one;
+            v = -1
+            for v, y in enumerate(conf_change):
+                if y == 1:
+                    break
+
+            c.remove(v)
+            tabulist.remove(v)
+
+            for n in g.neighbors[v]:
+                conf_change[n] = 1
+
+            # TODO
+            # w(e ) ← w(e ) + 1 for each uncovered edge e, and for its endpoints (x, y ), confChange(x ) ← 1 and
+            # confChange(y ) ← 1
+
+        # remove redundant vertices from C;
+        if cost(c, g) < cost(c_star, g):
+            c_star = c
+
+    return c
 
 
 if __name__ == '__main__':
     construct_tries = 100
     gr = generate_graph(30)
+    cutoff = 15
     fast_wvc(gr)
